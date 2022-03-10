52521618172f58e00971b32d6103e72df81cbbd0
Laura <laurapires@cpejr.com.br>
2022-03-10 16:27:25 -0300

    feat: Rotas User e Note

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,15 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+//Users
+Route.get('/users', 'UsersController.index')
+Route.post('/users', 'UsersController.store')
+Route.put('/users/:id', 'UsersController.update')
+Route.delete('/users/:id', 'UsersController.destroy')
+
+//Notes
+Route.get('/notes', 'NotesController.index')
+Route.post('/notes', 'NotesController.store')
+Route.put('/notes/:id', 'NotesController.update')
+Route.delete('/notes/:id', 'NotesController.destroy')
