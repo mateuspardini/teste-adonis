87656bdd8a9a87b369efef40b49e29be130c2900
Maria <mariamsilva15@ufmg.br>
2022-03-11 18:21:00 -0300

    fix: função index controllers de notes e users

--- app/Controllers/Http/NotesController.ts
+++ app/Controllers/Http/NotesController.ts
@@ -1,36 +1,35 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Note from 'App/Models/Note';
-
+import Note from 'App/Models/Note'
 
 export default class NotesController {
-async index() {
-  const note = await Note.all();
-  
-  return note;
-}
+  public async index() {
+    const note = await Note.all()
+
+    return note
+  }
 
   public async store({ request }: HttpContextContract) {
     const title = request.input('title')
     const note = request.input('note')
     const user_id = request.input('user_id')
-    
+
     const noteData = await Note.create({
       title,
       note,
-      user_id
+      user_id,
     })
     return noteData
   }
 
   public async update({ params, request }) {
-    const note = await Note.findOrFail(params.id);
-    const data = request.only(["title", "note","user_id"]);
-    
-    note.merge(data);
-    await note.save();
-    
+    const note = await Note.findOrFail(params.id)
+    const data = request.only(['title', 'note', 'user_id'])
+
+    note.merge(data)
+    await note.save()
+
     return note
-}
+  }
 
   public async destroy({ request }: HttpContextContract) {
     const id = request.param('id')
--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -2,34 +2,34 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class UsersController {
-async index() {
-  const user = await User.all();
-  
-  return user;
-}
+  public async index() {
+    const user = await User.all()
+
+    return user
+  }
 
   public async store({ request }: HttpContextContract) {
     const name = request.input('name')
     const email = request.input('email')
     const password = request.input('password')
-    
+
     const user = await User.create({
       name,
       email,
-      password
+      password,
     })
     return user
   }
 
   public async update({ params, request }) {
-    const user = await User.findOrFail(params.id);
-    const data = request.only(["name", "email","password"]);
-    
-    user.merge(data);
-    await user.save();
-    
+    const user = await User.findOrFail(params.id)
+    const data = request.only(['name', 'email', 'password'])
+
+    user.merge(data)
+    await user.save()
+
     return user
-}
+  }
 
   public async destroy({ request }: HttpContextContract) {
     const id = request.param('id')
